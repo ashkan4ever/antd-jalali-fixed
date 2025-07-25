ef7f940eaf6bd069d6ff3a59e3441c9208df500e
Mohammad Zohrabi <m@Mohammads-MacBook-Pro.local>
2025-07-25 13:41:52 +0330

    added new workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { ghPages } from "vite-plugin-gh-pages";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [ghPages()],
   base: "/antd-jalali-fixed/",
